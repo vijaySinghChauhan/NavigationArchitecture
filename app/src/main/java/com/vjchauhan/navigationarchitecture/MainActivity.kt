8d72a87a3805150bb5c311a3504d6cd122a6ebc9
vijay chauhan <>
2022-08-06 01:40:21 +0530

    Navigation done

--- app/src/main/java/com/vjchauhan/navigationarchitecture/MainActivity.kt
+++ app/src/main/java/com/vjchauhan/navigationarchitecture/MainActivity.kt
@@ -2,10 +2,14 @@ package com.vjchauhan.navigationarchitecture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.vjchauhan.navigationarchitecture.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
     }
 }
---
+++ app/src/main/java/com/vjchauhan/navigationarchitecture/SecondFragment.kt
@@ -0,0 +1,34 @@
+package com.vjchauhan
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.vjchauhan.navigationarchitecture.R
+import com.vjchauhan.navigationarchitecture.databinding.FragmentSecondBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SecondFragment : Fragment() {
+
+    private lateinit var binding: FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false)
+
+        var input = requireArguments().getString("user_input")
+        binding.textView.text = input.toString()
+
+        return binding.root
+    }
+
+
+}
